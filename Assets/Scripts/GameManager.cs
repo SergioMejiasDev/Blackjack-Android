58ea7e8eb979985b54a2a14f0c2d998683ab07a6
Sergio Mejias <sergiomejias.dev@gmail.com>
2021-05-14 00:50:42 +0200

    Update 2021.0514
    - Added letterbox system.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,11 +35,9 @@ public class GameManager : MonoBehaviour
     {
         manager = this;
 
-        CheckVolume();
+        LetterBoxer.AddLetterBoxingCamera();
 
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
-
-        ScaleScreen();
     }
 
     /// <summary>
@@ -206,43 +204,6 @@ public class GameManager : MonoBehaviour
         }
 
         SaveManager.saveManager.SaveOptions();
-    }
-
-    /// <summary>
-    /// Function called to scale the screen to a 16:9 format.
-    /// </summary>
-    void ScaleScreen()
-    {
-        float targetAspect = 16.0f / 9.0f;
-        float windowAspect = (float)Screen.width / (float)Screen.height;
-        float scaleheight = windowAspect / targetAspect;
-        Camera camera = Camera.main;
-
-        if (scaleheight < 1.0f)
-        {
-            Rect rect = camera.rect;
-
-            rect.width = 1.0f;
-            rect.height = scaleheight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleheight) / 2.0f;
-
-            camera.rect = rect;
-        }
-
-        else
-        {
-            float scalewidth = 1.0f / scaleheight;
-
-            Rect rect = camera.rect;
-
-            rect.width = scalewidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scalewidth) / 2.0f;
-            rect.y = 0;
-
-            camera.rect = rect;
-        }
     }
 
     /// <summary>
